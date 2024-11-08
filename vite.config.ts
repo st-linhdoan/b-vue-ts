33c8cf63daf374c78eb78cc2db545555e57a3967
Linh Doan T. B <linh.doan@MBAM0157.local>
2024-11-08 14:36:51 +0700

    Config i18n and alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,24 @@
-import { fileURLToPath, URL } from 'node:url'
+import Path from 'path';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
 
 export default defineConfig({
   plugins: [vue(), vueJsx()],
   server: {
-    port: 3000 // Change this to your desired port
+    port: 3000, // Change this to your desired port
   },
+  publicDir: './src/assets',
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      '@': Path.resolve(__dirname, './src'),
+      '@app': Path.resolve(__dirname, './src/app'),
+      '@config': Path.resolve(__dirname, './src/config'),
+      '@stylesheet': Path.resolve(__dirname, './src/stylesheet'),
+      '@shared': Path.resolve(__dirname, './src/app/shared'),
+      '@core': Path.resolve(__dirname, './src/app/core'),
+      '@assets': Path.resolve(__dirname, './src/assets'),
+    },
+  },
+});
